97e59f72b17dfc4f2be2746bec8a59350cd85103
Viktor Korsnes <viktor.kornses@gmail.com>
2018-02-13 11:52:04 +0100

    Added delay, door and initialize
    Signed-off-by: Viktor Korsnes <viktor.kornses@gmail.com>

--- heisdriver/main.c
+++ heisdriver/main.c
@@ -1,6 +1,40 @@
-#include "elev.h"
 #include <stdio.h>
+#include <time.h>
+#include "elev.h"
 
+
+
+void omvk_initialize()
+{
+    if (elev_get_floor_sensor_signal() == -1)
+        elev_set_motor_direction(DIRN_DOWN);
+    else
+        elev_set_motor_direction(DIRN_STOP); 
+    while (1) {
+        // Change direction when we reach top/bottom floor
+        if (elev_get_floor_sensor_signal() !=  -1) {
+            elev_set_motor_direction(DIRN_STOP);
+            elev_set_floor_indicator(elev_get_floor_sensor_signal());
+            break;
+        }
+                if (elev_get_stop_signal() == 1) {
+            elev_set_motor_direction(DIRN_STOP);
+            break;
+        }
+    }
+}
+void omvk_delay(int s)
+{
+    time_t t = time(NULL);
+    while((t + s) > (time(NULL))){}
+}
+
+void omvk_doors()
+{
+    elev_set_door_open_lamp(1);
+    omvk_delay(3);
+    elev_set_door_open_lamp(0);
+}
 
 int main() {
     // Initialize hardware
@@ -8,21 +42,15 @@ int main() {
         printf("Unable to initialize elevator hardware!\n");
         return 1;
     }
-
-    printf("Press STOP button to stop elevator and exit program.\n");
-
-    elev_set_motor_direction(DIRN_UP);
+    int currentFloor = -1;
+    omvk_initialize();
+    elev_set_door_open_lamp(0);
+    omvk_doors();
+    currentFloor = elev_get_floor_sensor_signal();
 
     while (1) {
-        // Change direction when we reach top/bottom floor
-        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
-            elev_set_motor_direction(DIRN_DOWN);
-        } else if (elev_get_floor_sensor_signal() == 0) {
-            elev_set_motor_direction(DIRN_UP);
-        }
-
         // Stop elevator and exit program if the stop button is pressed
-        if (elev_get_stop_signal()) {
+        if (elev_get_stop_signal() == 1) {
             elev_set_motor_direction(DIRN_STOP);
             break;
         }
