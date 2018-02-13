c60558a1dffb3d005b4424ba370e37fccafc008d
Viktor Korsnes <viktor.kornses@gmail.com>
2018-02-13 13:53:54 +0100

    Fikset bestillingsknapper
    Signed-off-by: Viktor Korsnes <viktor.kornses@gmail.com>

--- heisdriver/main.c
+++ heisdriver/main.c
@@ -2,18 +2,50 @@
 #include <time.h>
 #include "elev.h"
 
-typedef enum {
-    STATE_MOVING_DOWN,
-    STATE_MOVING_UP,
-    STATE_STOPPED
-} states;
+int order_array[4][3] = {
+    {0,0,0},
+    {0,0,0},
+    {0,0,0},
+    {0,0,0}
+};
 
-typedef enum {
-    STOP,
-    DIR_DOWN,
-    DIR_UP,
-    DOORS
-} action;
+elev_button_type_t buttonz[3] = {BUTTON_CALL_UP, BUTTON_COMMAND, BUTTON_CALL_DOWN};
+
+
+void omvk_initialize();
+
+void omvk_floor_buttons();
+
+void omvk_delay(int s);
+
+
+void omvk_doors();
+
+int main() {
+    // Initialize hardware
+    if (!elev_init()) {
+        printf("Unable to initialize elevator hardware!\n");
+        return 1;
+    }
+    int currentFloor = -1;
+    omvk_initialize();
+    elev_set_door_open_lamp(0);
+    omvk_doors();
+    currentFloor = elev_get_floor_sensor_signal();
+
+    while (1) {
+        omvk_floor_buttons();
+        //elev_set_button_lamp(elev_get_button_signal());
+
+        // Stop elevator and exit program if the stop button is pressed
+        if (elev_get_stop_signal() == 1) {
+            elev_set_motor_direction(DIRN_STOP);
+            break;
+        }
+    }
+
+    return 0;
+}
 
 void omvk_initialize()
 {
@@ -34,6 +66,49 @@ void omvk_initialize()
         }
     }
 }
+
+void omvk_floor_buttons()
+{
+    for (int i = 0; i < 4; ++i) {
+        switch (i) {
+            case 0: {
+                for (int j = 0; j < 2; ++j) {
+                    if (elev_get_button_signal(buttonz[j], i)){
+                        elev_set_button_lamp(buttonz[j], i, 1);
+                        order_array[i][j] = 1;
+                    }
+
+                }
+            } break;
+            case 1: {
+                for (int j = 0; j < 3; ++j) {
+                    if (elev_get_button_signal(buttonz[j], i)) {
+                        elev_set_button_lamp(buttonz[j], i, 1);
+                        order_array[i][j] = 1;
+                    }
+                }
+            } break;
+            case 2: {
+                for (int j = 0; j < 3; ++j) {
+                    if (elev_get_button_signal(buttonz[j], i)) {
+                        elev_set_button_lamp(buttonz[j], i, 1);
+                        order_array[i][j] = 1;
+                    }
+                }
+            } break;
+            case 3: {
+                for (int j = 1; j < 3; ++j) {
+                    if (elev_get_button_signal(buttonz[j], i)) {
+                        elev_set_button_lamp(buttonz[j], i, 1);
+                        order_array[i][j] = 1;
+                    }
+                }
+            } break;
+        }
+    }
+
+}
+
 void omvk_delay(int s)
 {
     time_t t = time(NULL);
@@ -45,46 +120,4 @@ void omvk_doors()
     elev_set_door_open_lamp(1);
     omvk_delay(3);
     elev_set_door_open_lamp(0);
-}
-
-int main() {
-    // Initialize hardware
-    if (!elev_init()) {
-        printf("Unable to initialize elevator hardware!\n");
-        return 1;
-    }
-    int currentFloor = -1;
-    omvk_initialize();
-    elev_set_door_open_lamp(0);
-    omvk_doors();
-    currentFloor = elev_get_floor_sensor_signal();
-    elev_set_motor_direction(DIRN_DOWN);
-
-    while (1) {
-        if (elev_get_floor_sensor_signal() == 0) {
-            elev_set_motor_direction(DIRN_STOP);
-            elev_set_floor_indicator(elev_get_floor_sensor_signal());
-            omvk_doors();
-            elev_set_motor_direction(DIRN_UP);
-            omvk_delay(1);
-        }
-        else if (elev_get_floor_sensor_signal() == 3) {
-            elev_set_motor_direction(DIRN_STOP);
-            elev_set_floor_indicator(elev_get_floor_sensor_signal());
-            omvk_doors();
-            elev_set_motor_direction(DIRN_DOWN);
-            omvk_delay(1);
-        }
-        else if (elev_get_floor_sensor_signal() != -1) {
-            elev_set_floor_indicator(elev_get_floor_sensor_signal());
-        }
-
-        // Stop elevator and exit program if the stop button is pressed
-        if (elev_get_stop_signal() == 1) {
-            elev_set_motor_direction(DIRN_STOP);
-            break;
-        }
-    }
-
-    return 0;
 }
