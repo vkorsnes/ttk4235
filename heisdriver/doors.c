0b4c328698c0f4d90516363ee9018b1e77cc2b69
Viktor Korsnes <viktor.korsnes@gmail.com>
2018-03-11 14:35:39 +0100

    Last polish before delivery of code
    Signed-off-by: Viktor Korsnes <viktor.korsnes@gmail.com>

--- heisdriver/doors.c
+++ heisdriver/doors.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
+#include <time.h>
 #include "doors.h"
 #include "buttons.h"
-#include <time.h>
 
 void doors()
 {
     elev_set_door_open_lamp(1);
 
-    //Start gets a timespamp from CPU clock, dividing by the constant "CLOCKS_PER_SEC" turns the value into seconds
-    //Program then checks for new orders while door is open
+    // Start gets a timestamp from CPU clock, dividing by the constant "CLOCKS_PER_SEC" turns the value into seconds
+    // Program then checks for new orders while door is open
     clock_t start = clock()/CLOCKS_PER_SEC;
-    while ((int)start+3 > (int)(clock()/CLOCKS_PER_SEC))
+    while ((int)(start+3) > (int)(clock()/CLOCKS_PER_SEC))
     	check_buttons_for_input();
     elev_set_door_open_lamp(0);
 }
